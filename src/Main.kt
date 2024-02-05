78490ff6adb5418ad61a44028823abd4cd1f7b7c
Lisis Ruschel <laruschel@proton.me>
2024-02-05 15:57:41 +0100

    fist exercise done

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Hello World!")
+    println()
+    println("Hallo, wie heißt du? (confirm with enter)")
+    var myName = readln()
+    println()
+    println("Hello $myName!")
+
+    println()
+    println("Wie alt bist du? (confirm with enter)")
+    var age = readln()
+    println()
+    println("Wow! Bereits $age Jahre?!")
+}
